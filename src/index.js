532c9f837d70977f4afc1d89c52b5815699fcfb5
Oksana07 <79578774+Oksana07@users.noreply.github.com>
2021-08-31 00:10:59 +0300

    домашня на практиці

--- src/index.js
+++ src/index.js
@@ -1,1 +1,56 @@
 
+import { enable } from "debug";
+import { async } from "fast-glob";
+import { add } from "lodash";
+import { even } from "prelude-ls";
+import { getPictures } from "./services/apiService";
+import cardTmp from './templates/cardTmp.hbs'
+
+// getPictures('car', '1')
+
+const refs = {
+    form: document.querySelector('#search-form'),
+    gallery: document.querySelector('.gallery'),
+    loadMore: document.querySelector('.load-more'),
+};
+
+const state = { page: 1, value: ""};
+refs.loadMore.style.visibility = 'hidden';
+
+refs.form.addEventListener('submit', onSearch);
+refs.loadMore.addEventListener('click', onLoadMore);
+
+
+async function onSearch(e){
+e.preventDefault();
+try {
+    state.value = e.currentTarget.elements.query.value
+    const pictures = await getPictures(state.value, state.page);
+    refs.gallery.innerHTML = cardTmp(pictures);
+    if (pictures.length > 11) {
+        refs.loadMore.style.visibility = "visible";
+    }
+
+} catch (error) {
+console.log(error.message);
+    }
+}
+
+// Load more
+
+
+async function onLoadMore(){
+state.page += 1
+const pictures = await getPictures(state.value, state.page);
+refs.gallery.insertAdjacentHTML('beforeend', cardTmp(pictures));
+
+refs.gallery.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
+}
+
+
+
+
+ 
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,4 +1,9 @@
 import axios from "axios";
 
-const MY_API_KEY ='23115827-a7cc928e2386c212e5b47dc23'
-axios.defaults.baseURL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal'
+const MY_API_KEY ='23115827-a7cc928e2386c212e5b47dc23';
+axios.defaults.baseURL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
+
+export async function getPictures(querry, page){
+    const {data:{hits},} = await axios.get(`&q=${querry}&page=${page}&per_page=12&key=${MY_API_KEY}`);
+return hits;
+}
